455814e95c8fb89fdcbe1e372a6d08530174e7b8
Matt Foster <matt.p.foster@gmail.com>
2008-11-14 14:38:13 +0000

    added applescript to allow Growl notifications -- using applescript instead of growl means we dont need to install another library

--- Support/lib/ipythonMate.py
+++ Support/lib/ipythonMate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 # Interact with Terminal.app using applescript
 
 import os
@@ -19,8 +20,42 @@ def run_in_terminal(text, proc_name='Python', app_name='Terminal', command='ipyt
   APPLESCRIPT""" % (app_name, proc_name, command, text)
   os.system(cmd)
   
+def notify_with_growl(title, description):
+    cmd = """osascript <<- APPLESCRIPT
+    tell application "GrowlHelperApp"
+    	-- Make a list of all the notification types 
+    	-- that this script will ever send:
+    	set the allNotificationsList to ¬
+    		{"ipythonMate"}
+
+    	-- Make a list of the notifications 
+    	-- that will be enabled by default.      
+    	-- Those not enabled by default can be enabled later 
+    	-- in the 'Applications' tab of the growl prefpane.
+    	set the enabledNotificationsList to ¬
+    		{"ipythonMate"}
+
+    	-- Register our script with growl.
+    	-- You can optionally (as here) set a default icon 
+    	-- for this script's notifications.
+    	register as application ¬
+    		"TextMate" all notifications allNotificationsList ¬
+    		default notifications enabledNotificationsList ¬
+    		icon of application "TextMate"
+
+    	--	Send a Notification...
+    	notify with name ¬
+    		"ipythonMate" title ¬
+    		"%s" description ¬
+    		"%s" application name "TextMate"
+    end tell
+
+    APPLESCRIPT""" % (title, description)
+    os.system(cmd)
+    
 if __name__ == '__main__':
-  run_in_terminal("ls")
+  # run_in_terminal("ls")
+  notify_with_growl("Ran IPython Script", "test")
   
 
   
