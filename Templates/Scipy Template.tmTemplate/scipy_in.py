aa1135c7722c843d907b96e22a02380a51470c2a
Matt Foster <matt.p.foster@gmail.com>
2008-12-08 22:32:08 +0000

    Added xrange snippet, and simple scipy template

---
+++ Templates/Scipy Template.tmTemplate/scipy_in.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+## 
+## ${TM_NEW_FILE_BASENAME}
+## 
+## Created by ${TM_FULLNAME} on ${TM_DATE}.
+## Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+## 
+
+"""
+${TM_NEW_FILE_BASENAME}
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pylab as plt
+
